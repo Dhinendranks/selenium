546e30e45e37dd2579abb4a9313de9877235eb52
Dhinendranks <147038043+Dhinendranks@users.noreply.github.com>
2026-05-26 11:50:02 +0530

    Add initial Amazon WebDriver automation script

---
+++ amazon.java
@@ -0,0 +1,26 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver ();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+	
+	   driver.findElement(By.id("nav-hamburger-menu")).click();
+	    
+	   Thread.sleep(3000);
+	   driver.findElement(By.id("hmenu-close-icon")).click();
+	    
+	  // driver.findElement(By.id("field-keywords"));
+	    
+	    Thread.sleep(4000);
+	    driver.quit();
+	}
+
+}
